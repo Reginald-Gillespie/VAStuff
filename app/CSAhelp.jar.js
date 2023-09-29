8778fab5ddf313b09a0441a6bf3cea196a6a9610
WKoA <109245101+Reginald-Gillespie@users.noreply.github.com>
2023-09-29 17:44:21 -0500

    Add files via upload

---
+++ app/CSAhelp.jar.js
@@ -0,0 +1,8 @@
+/*Compiled using CheerpJ (R) 2.3 by Leaning Technologies Ltd*/
+cheerpjCL={cl:null};
+function _c4pY3AppC2E0(a0,p){var a={p:p,f:_c4pY3AppC2E0,l0:a0,s0:null};a.pc=0;a.s0=a.l0;a.pc=-88;_n4VYObjectC2E0(a.s0,a);return;}function _c4_Y3App4mainE1(a0,p){var a={p:p,f:_c4_Y3App4mainE1,l0:a0,s0:null,s1:null};a.pc=0;;if((N4java4lang6SystemG|0)==0){cjG(a);}a.s0=N4java4lang6System.out1;a.pc=-161;a.s1=cheerpjInternString("Hello world");a.pc=-211;a.s0.v36(a.s0,a.s1,a);return;}var N3AppG;
+function N3App(){N4java4lang6Object.call(this);}
+N3App.cl=cheerpjCL;
+function N3AppX(a){a.f=N3AppX;if(cheerpjSafeInitGuard('N3App')) return;var q=a.q=N3App.prototype=Object.create(N4java4lang6Object.prototype);q.constructor=N3App;N3AppG=1;cheerpjSafeInitFinish('N3App');}
+var N4java4lang6ObjectG;
+var N4java4lang6SystemG;
